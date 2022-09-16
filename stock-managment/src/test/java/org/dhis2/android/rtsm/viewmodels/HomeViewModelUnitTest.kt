d4ca2f78a7fe5feecc81f5c88d72caa21108f977
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-09-16 10:46:57 +0200

    Mocking syncManagerVariable

--- stock-managment/src/test/java/org/dhis2/android/rtsm/viewmodels/HomeViewModelUnitTest.kt
+++ stock-managment/src/test/java/org/dhis2/android/rtsm/viewmodels/HomeViewModelUnitTest.kt
@@ -21,6 +21,7 @@ import org.dhis2.android.rtsm.data.OperationState
 import org.dhis2.android.rtsm.data.TransactionType
 import org.dhis2.android.rtsm.exceptions.UserIntentParcelCreationException
 import org.dhis2.android.rtsm.services.MetadataManager
+import org.dhis2.android.rtsm.services.SyncManager
 import org.dhis2.android.rtsm.services.UserManager
 import org.dhis2.android.rtsm.services.UserManagerImpl
 import org.dhis2.android.rtsm.services.preferences.PreferenceProvider
@@ -85,9 +86,13 @@ class HomeViewModelUnitTest {
     @Mock
     private lateinit var preferenceProvider: PreferenceProvider
 
+    @Mock
+    private lateinit var syncManager: SyncManager
+
     @Captor
     private lateinit var facilitiesArgumentCaptor:
         ArgumentCaptor<OperationState<List<OrganisationUnit>>>
+
 
     @Captor
     private lateinit var destinationsArgumentCaptor: ArgumentCaptor<OperationState<List<Option>>>
@@ -125,7 +130,8 @@ class HomeViewModelUnitTest {
             schedulerProvider,
             preferenceProvider,
             metadataManager,
-            getStateHandle()
+            getStateHandle(),
+            syncManager
         )
 
         viewModel.facilities.observeForever(facilitiesObserver)
