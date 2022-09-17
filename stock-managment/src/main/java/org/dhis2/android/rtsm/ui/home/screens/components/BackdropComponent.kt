b726d312611a84e1527586112cb92f5f1caa5cb1
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-09-17 07:04:48 +0200

    Compose backdrop component

---
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
@@ -0,0 +1,53 @@
+package org.dhis2.android.rtsm.ui.home.screens.components
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BackdropValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.material.rememberBackdropScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.dhis2.android.rtsm.ui.home.HomeViewModel
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Backdrop(
+    activity: Activity,
+    viewModel: HomeViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    themeColor: Color
+) {
+    val backdropState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+
+    BackdropScaffold(
+        appBar = {
+            Toolbar(
+                viewModel.toolbarTitle.collectAsState().value.name,
+                viewModel.fromFacility.collectAsState().value,
+                viewModel.deliveryTo.collectAsState().value,
+                themeColor,
+                navigationAction = {
+                    activity.finish()
+                },
+                backdropState
+            )
+        },
+        backLayerBackgroundColor = themeColor,
+        backLayerContent = {
+            FilterList(viewModel, themeColor)
+        },
+        frontLayerElevation = 5.dp,
+        frontLayerContent = {
+            Column(Modifier.padding(16.dp)) {
+                Text(text = "Main content")
+            }
+        },
+        scaffoldState = backdropState,
+        gesturesEnabled = false
+    )
+}
