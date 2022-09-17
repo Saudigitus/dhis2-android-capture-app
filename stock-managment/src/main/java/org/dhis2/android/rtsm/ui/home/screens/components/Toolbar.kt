3d9d741d5e65b5b9d5ae37013d0c3ce98adf4e2d
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-09-17 06:49:09 +0200

    Implement subtitle prototype color and right arrow between facilities and delivery to

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/Toolbar.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/Toolbar.kt
@@ -3,6 +3,7 @@ package org.dhis2.android.rtsm.ui.home.screens.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BackdropScaffoldState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -30,7 +32,8 @@ import org.dhis2.android.rtsm.utils.Utils.Companion.capitalizeText
 @Composable
 fun Toolbar(
     title: String,
-    subtitle: String,
+    from: String,
+    to: String?,
     themeColor: Color,
     navigationAction: () -> Unit,
     backdropState: BackdropScaffoldState
@@ -44,19 +47,44 @@ fun Toolbar(
                 horizontalAlignment = Alignment.Start
             ) {
                 Text(
-                    text = capitalizeText(title).ifBlank { "Home" },
+                    text = capitalizeText(title).ifBlank {
+                        stringResource(R.string.title_activity_home)
+                    },
                     style = MaterialTheme.typography.subtitle1,
                     maxLines = 1,
                     fontSize = 20.sp
                 )
-                Text(
-                    text = subtitle,
-                    style = MaterialTheme.typography.subtitle2,
-                    softWrap = true,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1,
-                    fontSize = 12.sp
-                )
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = from,
+                        style = MaterialTheme.typography.subtitle2,
+                        softWrap = true,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        fontSize = 12.sp,
+                        color = colorResource(R.color.toolbar_subtitle)
+                    )
+                    if (to != null) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_from_to),
+                            contentDescription = null,
+                            Modifier.padding(5.dp),
+                            tint = colorResource(R.color.toolbar_subtitle)
+                        )
+                        Text(
+                            text = to,
+                            style = MaterialTheme.typography.subtitle2,
+                            softWrap = true,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            fontSize = 12.sp,
+                            color = colorResource(R.color.toolbar_subtitle)
+                        )
+                    }
+                }
             }
         },
         navigationIcon = {
