2afd6cfe79cc1636d98b6647de23e1b237fbe08c
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-10-06 16:44:30 +0200

    Update DropdownComponent.kt

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/DropdownComponent.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/DropdownComponent.kt
@@ -57,7 +57,6 @@ import org.dhis2.android.rtsm.utils.Utils.Companion.capitalizeText
 import org.dhis2.commons.orgunitdialog.CommonOrgUnitDialog
 import org.hisp.dhis.android.core.option.Option
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnit
-import timber.log.Timber
 
 var orgUnitData: OrganisationUnit? = null
 var orgUnitName: String? = null
@@ -501,5 +500,4 @@ fun openOrgUnitTreeSelector(
     if (!orgUnitDialog.isAdded) {
         orgUnitDialog.show(supportFragmentManager, "")
     }
-
 }
