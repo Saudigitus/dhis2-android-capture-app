b7ac7315053440d27e24b7b7e9fa0f4282b55619
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-11-04 14:34:03 +0200

    moving mapTableModel to ManageStockViewModel.kt

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
@@ -5,38 +5,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import com.google.android.material.composethemeadapter.MdcTheme
 import org.dhis2.android.rtsm.R
 import org.dhis2.android.rtsm.ui.managestock.ManageStockViewModel
 import org.dhis2.composetable.model.KeyboardInputType
 import org.dhis2.composetable.model.TableCell
-import org.dhis2.composetable.model.TableHeader
-import org.dhis2.composetable.model.TableHeaderCell
-import org.dhis2.composetable.model.TableHeaderRow
-import org.dhis2.composetable.model.TableModel
-import org.dhis2.composetable.model.TableRowModel
 import org.dhis2.composetable.model.TextInputModel
 import org.dhis2.composetable.ui.DataSetTableScreen
-
-@Composable
-private fun mapTableModel(
-    stocks: List<TableRowModel>
-) = mutableListOf(
-    TableModel(
-        id = "STOCK",
-        tableHeaderModel = TableHeader(
-            rows = mutableListOf(
-                TableHeaderRow(
-                    mutableListOf(
-                        TableHeaderCell(stringResource(R.string.stock)),
-                        TableHeaderCell(stringResource(R.string.quantity))
-                    )
-                )
-            )
-        ),
-        tableRows = stocks,
-        upperPadding = false
-    )
-)
 
 @Composable
 fun ManageStockTable(
@@ -44,23 +19,27 @@ fun ManageStockTable(
 ) {
     val context = LocalContext.current
 
-    DataSetTableScreen(
-        tableData = mapTableModel(
-            viewModel.tableRowData(viewModel.getStockItems().observeAsState())
-        ),
-        onCellClick = { _, cell ->
-            onCellClick(cell)
-        },
-        onEdition = { isEditing ->
-            editingCellValue(context, isEditing)
-        },
-        onCellValueChange = { cell ->
-            onCellValueChanged(cell)
-        },
-        onSaveValue = { cell ->
-            onSaveValueChange(cell)
-        }
-    )
+    MdcTheme {
+        DataSetTableScreen(
+            tableData = viewModel.tableRowData(
+                viewModel.getStockItems().observeAsState(),
+                stringResource(R.string.stock),
+                stringResource(R.string.quantity)
+            ),
+            onCellClick = { _, cell ->
+                onCellClick(cell)
+            },
+            onEdition = { isEditing ->
+                editingCellValue(context, isEditing)
+            },
+            onCellValueChange = { cell ->
+                onCellValueChanged(cell)
+            },
+            onSaveValue = { cell ->
+                onSaveValueChange(cell)
+            }
+        )
+    }
 }
 
 fun editingCellValue(context: Context, editing: Boolean) {}
