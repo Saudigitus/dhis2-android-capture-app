4fcd67d742a7b59edf6b7552e0205130d1c9a90a
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-11-21 16:08:54 +0200

    Writing logic to mke button visible

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/HomeScreen.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/HomeScreen.kt
@@ -2,13 +2,16 @@ package org.dhis2.android.rtsm.ui.home.screens
 
 import android.app.Activity
 import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Snackbar
@@ -20,16 +23,15 @@ import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.DarkGray
-import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,11 +41,13 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.journeyapps.barcodescanner.ScanOptions
 import kotlinx.coroutines.CoroutineScope
 import org.dhis2.android.rtsm.R
+import org.dhis2.android.rtsm.data.TransactionType
 import org.dhis2.android.rtsm.ui.home.HomeActivity
 import org.dhis2.android.rtsm.ui.home.HomeViewModel
 import org.dhis2.android.rtsm.ui.home.screens.components.Backdrop
 import org.dhis2.android.rtsm.ui.managestock.ManageStockViewModel
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HomeScreen(
     activity: Activity,
@@ -58,74 +62,59 @@ fun HomeScreen(
 ) {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
-    val purple200 = Color(0xFF757575)
-    val purple2000 = Color(0xFF616161)
-    var enabled by remember { mutableStateOf(true) }
+    var enabled by remember { mutableStateOf(false) }
 
     Scaffold(
         scaffoldState = scaffoldState,
         floatingActionButton = {
-
-            CompositionLocalProvider(
-                LocalRippleTheme provides
-                    if (enabled) LocalRippleTheme.current else NoRippleTheme
+            AnimatedVisibility(
+                visible = checkVisibility(viewModel),
+                enter = fadeIn(),
+                exit = fadeOut()
             ) {
+                CompositionLocalProvider(
+                    LocalRippleTheme provides
+                        if (enabled) LocalRippleTheme.current else NoRippleTheme
+                ) {
 
-                ExtendedFloatingActionButton(
-                    modifier = Modifier.height(60.dp),
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.arrow_forward),
-                            contentDescription = stringResource(R.string.proceed)
-                        )
-                    },
-                    text = { Text(stringResource(R.string.proceed)) },
-                    onClick = {
-                        enabled != enabled
-                        if (enabled) {
-                            proceedAction(scope, scaffoldState)
-                        }
-                    },
-//                    backgroundColor = if (enabled) MaterialTheme.colors.secondary else Gray,
-//                    backgroundColor = purple200,
-//                backgroundColor = Color.Unspecified,
-                contentColor = purple2000,
-//                contentColor = if (enabled) LocalContentColor.current.copy(alpha = LocalContentAlpha.current) else DarkGray,
-//                contentColor = themeColor,
-//                contentColor = colorResource(id = R.color.button_proceed_text_color),
-//                shape = RoundedCornerShape(10.dp)
-                )
-
-//                FloatingActionButton(
-//                    backgroundColor = if (enabled) MaterialTheme.colors.secondary else Gray,
-//                    onClick = { if (enabled) { /* do something */ } else {} },
-//                ) {
-//                    Icon(Icons.Filled.Favorite,
-//                        contentDescription = "Localized description",
-//                        tint = if (enabled)
-//                            LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-//                        else DarkGray)
-//                }
+                    ExtendedFloatingActionButton(
+                        modifier = Modifier
+                            .shadow(
+                                ambientColor = Color.Black.copy(alpha = 0.5f),
+                                clip = false,
+                                elevation = 0.dp
+                            )
+                            .height(70.dp)
+                            .animateEnterExit(
+                                enter = slideInHorizontally(),
+                                exit = slideOutHorizontally()
+                            ),
+                        icon = {
+                            Icon(
+                                painter = painterResource(R.drawable.proceed_icon),
+                                contentDescription = stringResource(R.string.proceed),
+                                tint = if (enabled) themeColor else colorResource(id = R.color.proceed_text_color)
+                            )
+                        },
+                        text = {
+                            Text(
+                                stringResource(R.string.proceed),
+                                color = if (enabled) themeColor else colorResource(id = R.color.proceed_text_color)
+                            )
+                        },
+                        onClick = {
+                            if (enabled) {
+                                enabled = !enabled
+                                proceedAction(scope, scaffoldState)
+                            } else {
+                                enabled = !enabled
+                            }
+                        },
+                        backgroundColor = if (enabled) Color.White else colorResource(id = R.color.proceed_color),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                }
             }
-
-
-//            ExtendedFloatingActionButton(
-//                modifier = Modifier.height(60.dp),
-//                icon = {
-//                    Icon(
-//                        painter = painterResource(R.drawable.arrow_forward),
-//                        contentDescription = stringResource(R.string.proceed)
-//                    )
-//                },
-//                text = { Text(stringResource(R.string.proceed)) },
-//                onClick = { proceedAction(scope, scaffoldState) },
-//                backgroundColor = purple200,
-////                backgroundColor = Color.Unspecified,
-////                contentColor = Color.LightGray,
-////                contentColor = themeColor,
-////                contentColor = colorResource(id = R.color.button_proceed_text_color),
-////                shape = RoundedCornerShape(10.dp)
-//            )
         },
         snackbarHost = {
             SnackbarHost(hostState = it) { data ->
@@ -159,4 +148,13 @@ private object NoRippleTheme : RippleTheme {
 
     @Composable
     override fun rippleAlpha(): RippleAlpha = RippleAlpha(0.0f, 0.0f, 0.0f, 0.0f)
+}
+
+@Composable
+fun checkVisibility(viewModel: HomeViewModel): Boolean {
+    return if ((viewModel.toolbarTitle.collectAsState().value.name == TransactionType.DISCARD.name)) {
+        return viewModel.hasFacilitySelected.collectAsState().value
+    } else if ((viewModel.toolbarTitle.collectAsState().value.name == TransactionType.CORRECTION.name)) {
+        return viewModel.hasFacilitySelected.collectAsState().value
+    } else ((viewModel.toolbarTitle.collectAsState().value.name == TransactionType.DISTRIBUTION.name) && viewModel.hasFacilitySelected.collectAsState().value && viewModel.hasDestinationSelected.collectAsState().value)
 }
