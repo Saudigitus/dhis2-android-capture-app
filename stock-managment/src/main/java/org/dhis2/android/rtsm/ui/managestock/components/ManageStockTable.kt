33e88759067ac272e375fc6e53f4a446bef302f8
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-12-13 11:46:31 +0200

    implementing editing cell logic

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
@@ -28,21 +28,11 @@ fun ManageStockTable(
                     viewModel.onCellClick(cell)
                 },
                 onEdition = { isEditing ->
-                    editingCellValue(isEditing, concealBackdropState)
+                    viewModel.editingCellValue(isEditing, concealBackdropState)
                 },
                 onCellValueChange = viewModel::onCellValueChanged,
                 onSaveValue = viewModel::onSaveValueChange
             )
         }
-    }
-}
-
-fun editingCellValue(
-    editing: Boolean,
-    onEditionStart: () -> Unit
-) {
-    // TODO Hide review button
-    if (editing) {
-        onEditionStart.invoke()
     }
 }
