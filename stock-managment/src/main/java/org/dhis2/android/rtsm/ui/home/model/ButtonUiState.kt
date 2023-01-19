80beaf57845acef083c4f82a3c06e791c9e1c6a7
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-01-19 14:58:48 +0200

    adding button props

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/model/ButtonUiState.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/model/ButtonUiState.kt
@@ -1,11 +1,13 @@
 package org.dhis2.android.rtsm.ui.home.model
 
+import androidx.compose.ui.graphics.Color
 import org.dhis2.android.rtsm.R
 import org.dhis2.android.rtsm.ui.home.model.ButtonVisibilityState.HIDDEN
 
 data class ButtonUiState(
     val text: Int = R.string.review,
     val icon: Int = R.drawable.proceed_icon,
+    val color: Color = Color.White,
     val visibility: ButtonVisibilityState = HIDDEN
 )
 
