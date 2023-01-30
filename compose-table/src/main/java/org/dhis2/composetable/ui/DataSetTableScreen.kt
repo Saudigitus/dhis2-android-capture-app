8b0271c8922626e9fab08313fd8965004e251bf5
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-01-30 15:06:16 +0200

    Androapp 5157 (#52)
    * Checking data
    
    * [ANDROAPP-5157] move complete actions to viewmodel
    
    * Pass dataEntryUiState as parameter
    
    * modify hasUnsavedData to dataEntryUiState
    
    * Update parameters
    
    * Show SnackBar on CommitTransaction
    
    In this commiit, i'm just summarizing all the information in one commit.
    
    * Update MainContent.kt
    
    * Creating new merge
    
    * Code Style
    
    * Update paperwork.json
    
    * Creating Ui State For SnackBar and pass the parameters
    
    * Fixing UI Style
    
    * [ANDROAPP-5157] Refactor completion dialog to it's own class
    
    * [ANDROAPP-5157] Manage back behaviour for Start step
    
    * [ANDROAPP-5157] remove transaction status
    
    ---------
    
    Co-authored-by: andresmr <personal@andresmr.com>

--- compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableScreen.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableScreen.kt
@@ -145,8 +145,7 @@ fun DataSetTableScreen(
     }
 
     BackHandler(
-        !tableScreenState.textInputCollapsedMode ||
-            bottomSheetState.bottomSheetState.isExpanded &&
+        bottomSheetState.bottomSheetState.isExpanded &&
             !bottomSheetState.bottomSheetState.isAnimationRunning
     ) {
         collapseBottomSheet(finish = true)
